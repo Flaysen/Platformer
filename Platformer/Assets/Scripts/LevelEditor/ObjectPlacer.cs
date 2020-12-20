2e677e391914fba5dd4765af4384230ebdf15449
Flaysen <kamiltuszkiewicz@gmail.com>
2020-12-20 12:13:27 +0100

    basic grid object placement

---
+++ Platformer/Assets/Scripts/LevelEditor/ObjectPlacer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelEditor 
+{
+    public class ObjectPlacer : MonoBehaviour
+    {
+        private Grid _grid;
+
+        private void Awake()
+        {
+            _grid = FindObjectOfType<Grid>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hitInfo;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if(Physics.Raycast(ray, out hitInfo))
+                {
+                    PlaceObjectNear(hitInfo.point);
+                }
+            }   
+        }
+
+        private void PlaceObjectNear(Vector3 clickPoint)
+        {
+            var finalPosition = _grid.GetNearestPointOnGrid(clickPoint);
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;  
+        }
+    }   
+}
+
